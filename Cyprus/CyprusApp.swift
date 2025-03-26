4966c0e948c1c0b179ff513e0802b82c9e8364fc
Mustafa <mustafajafridi@gmail.com>
2025-03-26 18:38:11 -0400

    Initial Commit

---
+++ Cyprus/CyprusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyprusApp.swift
+//  Cyprus
+//
+//  Created by Mustafa Junaid on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CyprusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CyprusTests/CyprusTests.swift
@@ -0,0 +1,17 @@
+//
+//  CyprusTests.swift
+//  CyprusTests
+//
+//  Created by Mustafa Junaid on 26/03/2025.
+//
+
+import Testing
+@testable import Cyprus
+
+struct CyprusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
